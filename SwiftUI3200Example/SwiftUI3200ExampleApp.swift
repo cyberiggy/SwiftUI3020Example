c031a4dd3f075f3d1d0816e1794d7a19289c39e3
Ron Woodbury <ronwoodbury@mac.com>
2021-11-15 18:27:38 -0700

    Initial Commit

---
+++ SwiftUI3200Example/SwiftUI3200ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3200ExampleApp.swift
+//  SwiftUI3200Example
+//
+//  Created by Ron Woodbury on 11/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3200ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
